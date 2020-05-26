dcdf7bf968713d97f4ea46ae19aaf4d7eda9b8cf
Takashi Miyajima <tam@tmsoftware.net>
2020-05-25 19:02:45 -0700

    Initial Release

--- README.md
+++ README.md
@@ -1,68 +1,38 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# COVID-19 Casemap Web
+Open source web app to display COVID-19 cumulative case data via easy-to-understand interactive map interface. Retrieve data from covid19api.tmsoftwareinc.com via RESTful API. 
+COVID-19 case data are
+* City data in Los Angels County, Oraneg County, Ventura County and San Diego County in the US
+* City data in state of Illinois in the US
+* County data in every state in the US
+* Country data all over the world
 
-## Available Scripts
+## Getting Started
+
+Install node modules
+
+```npm install```
+
+Setup Google Maps JavaScript API Key in .env file in project root folder
+
+```REACT_APP_MAP_API_KEY=XXXXXXXXXXXXXXXXXXXX```
 
 In the project directory, you can run:
 
-### `npm start`
+```npm start```
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Runs the app in the development mode.
+Open http://localhost:3000 to view it in the browser.
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+## Live App
+[https://covid19.tmsoftwareinc.com/](https://covid19.tmsoftwareinc.com/)
 
-### `npm test`
+## Built With
+* [react](https://reactjs.org/)
+* [google-maps-react](https://github.com/fullstackreact/google-maps-react)
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+* [recharts](https://github.com/recharts/recharts)
 
-### `npm run build`
+* [react-tabs](https://github.com/reactjs/react-tabs)
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+## License
+This project is licensed under [the MIT License](https://opensource.org/licenses/MIT)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,301 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { GoogleApiWrapper, Map, Marker, InfoWindow } from 'google-maps-react';
+import {LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer} from 'recharts';
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const API_LATLNG = 'https://covid19.tmsoftwareinc.com/api/v1/latlngs?lat='
+const API_ADR = 'https://covid19.tmsoftwareinc.com/api/v1/addresses?country='
+const API_GEO = 'https://covid19.tmsoftwareinc.com/api/v1/geos?location='
+
+class GoogleMap extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    // HW Upper School
+    lat: 34.139619,
+    lng: -118.412786,
+    city: null,
+    county: null,
+    state: null,
+    country: null,
+    date: null,
+    cases: null,
+    cityJsonData: null,
+    countyJsonData: null,
+    stateJsonData: null,
+    countryJsonData: null,
+    cityTabDisabled: false,
+    countyTabDisabled: false,
+    stateTabDisabled: false,
+    countryTabDisabled: false,
+    tabIndex: 0,
+    searchLocation: 'Harvard-Westlake Upper School',
+    usage: 'Usage: Click (Tap) to see data there.'
+  }
+
+  getCases() {
+    // this.state.lat, lng: this.state.lng
+    fetch(API_LATLNG+this.state.lat+'&long='+this.state.lng)
+    .then(res => res.json())
+    .then((data) => {
+      if(data.length === 0) {
+        this.setState({
+          city: null,
+          county: null,
+          state: null,
+          country: null,
+          date: null,
+          cases: 'No Data Found Here',
+          showingInfoWindow: true,
+          activeMarker: {},
+          cityJsonData: null,
+          countyJsonData: null,
+          stateJsonData: null,
+          countryJsonData: null,
+          tabIndex: 0,
+          cityTabDisabled: false,
+          countyTabDisabled: false,
+          stateTabDisabled: false,
+          countryTabDisabled: false,
+        });
+      }
+      else {
+        if(data[0]['country'] === 'us' && data[0]['county'] !=='New York City') {
+          this.setState({county: data[0]['county']+ ' County'})
+        }
+        else {
+          this.setState({county: data[0]['county']})
+        }
+  // city existence is assumed at first
+      this.setState({
+        city: data[0]['city'],
+  //      county: data[0]['county'],
+        state: data[0]['state'],
+        country: data[0]['country'],
+        date: data[0]['date'],
+        cases: data[0]['cases'] + ' cases',
+        showingInfoWindow: true,
+        activeMarker: {},
+        cityJsonData: data,
+        countyJsonData: null,
+        stateJsonData: null,
+        countryJsonData: null,
+        tabIndex: 0,
+        cityTabDisabled: false,
+        countyTabDisabled: false,
+        stateTabDisabled: false,
+        countryTabDisabled: false,
+      });
+ // Move json Data step by step     
+      if(data[0]['city'] === '') {
+        this.setState({
+          tabIndex: 1,
+          cityTabDisabled: true,
+          cityJsonData: null,
+          countyJsonData: data
+        });
+        if(data[0]['county'] === '') {
+          this.setState({
+            tabIndex: 2,
+            countyTabDisabled: true,
+            countyJsonData: null,
+            stateJsonData: data
+          });
+          if(data[0]['state'] === '') {
+            this.setState({
+              tabIndex: 3,
+              stateTabDisabled: true,
+              stateJsonData: null,
+              countryJsonData: data
+            });
+          }
+          else {
+            this.setState({
+              countryTabDisabled: true
+            });
+          }
+        } 
+        else {
+// Get state data
+          fetch(API_ADR+data[0]['country']+'&state='+data[0]['state'])
+          .then(res3 => res3.json())
+          .then((data3) => {
+            this.setState({
+              countryTabDisabled: true,
+              stateJsonData: data3
+            });
+          });
+        }
+      }
+      else {
+        // Get county data
+        fetch(API_ADR+data[0]['country']+'&state='+data[0]['state']+'&county='+data[0]['county'])
+        .then(res2 => res2.json())
+        .then((data2) => {
+        this.setState({
+          tabIndex: 0,
+          countyJsonData: data2
+          });
+        });
+
+        // Get state data
+        fetch(API_ADR+data[0]['country']+'&state='+data[0]['state'])
+        .then(res3 => res3.json())
+        .then((data3) => {
+        console.log(data3)
+        this.setState({
+          tabIndex: 0,
+          countryTabDisabled: true,
+          stateJsonData: data3
+          });
+        });
+      }
+      }
+    })
+    .catch(console.log)
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      });
+      this.getCases();
+    },
+    (err) => {
+      this.getCases();
+      console.log(err);
+    })
+  }
+
+  mapClicked = (mapProps, map, event) => {
+    const { markers } = this.state;
+    const lat = event.latLng.lat();
+    const lng = event.latLng.lng();
+    this.setState({
+      lat: lat,
+      lng: lng
+    });
+    this.getCases();
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      activeMarker: marker
+    });
+  }
+
+  onSearchLocationKeyPress = (event) => {
+    if(event.keyCode == 13) {
+      this.onSearchButtonClick();
+    }
+  }
+
+  onSearchLocationChange = (event) => {
+    this.setState({
+      searchLocation: event.target.value 
+    });
+  }
+
+  onSearchButtonClick = (event) => {
+    fetch(API_GEO+this.state.searchLocation)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({
+        lat: data['lat'],
+        lng: data['lng'],
+        showingInfoWindow: false
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div class="w-section section ">
+        <h5>
+        Click (Tap) on the map to see total cases there.
+        </h5>
+        
+        <div class="w-container centering">
+        <input type='text' size="40" placeholder="City, State, Country" onChange={this.onSearchLocationChange} onKeyDown={this.onSearchLocationKeyPress} />&nbsp;
+        <button class="button small-button" onClick={this.onSearchButtonClick}>Search</button>
+        </div>
+    
+      </div>
+      <div class="w-section section purple" id="chart">
+      <Tabs selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })} >
+      <TabList>
+      <Tab disabledClassName="react-tabs__tab--disabled-hidden" disabled={this.state.cityTabDisabled} >{this.state.city}</Tab>
+      <Tab disabledClassName="react-tabs__tab--disabled-hidden" disabled={this.state.countyTabDisabled}>{this.state.county}</Tab>
+      <Tab disabledClassName="react-tabs__tab--disabled-hidden" disabled={this.state.stateTabDisabled}>{this.state.state}</Tab>
+      <Tab disabledClassName="react-tabs__tab--disabled-hidden" disabled={this.state.countryTabDisabled}>{this.state.country}</Tab>
+    </TabList>
+      <TabPanel>
+      <ResponsiveContainer width="95%" height={200}>
+        <LineChart data={this.state.cityJsonData}>
+        <XAxis dataKey="date" reversed/>
+        <YAxis/>
+        <Line type="monotone" dataKey="cases" stroke="#8884d8" />
+        <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+      </TabPanel>
+      <TabPanel>
+      <ResponsiveContainer width="95%" height={200}>
+        <LineChart data={this.state.countyJsonData}>
+        <XAxis dataKey="date" reversed/>
+        <YAxis/>
+        <Line type="monotone" dataKey="cases" stroke="#8884d8" />
+        <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+    </TabPanel>
+    <TabPanel>
+      <ResponsiveContainer width="95%" height={200}>
+        <LineChart data={this.state.stateJsonData}>
+        <XAxis dataKey="date" reversed/>
+        <YAxis/>
+        <Line type="monotone" dataKey="cases" stroke="#8884d8" />
+        <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+    </TabPanel>
+    <TabPanel>
+      <ResponsiveContainer width="95%" height={200}>
+        <LineChart data={this.state.countryJsonData}>
+        <XAxis dataKey="date" reversed/>
+        <YAxis/>
+        <Line type="monotone" dataKey="cases" stroke="#8884d8" />
+        <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+    </TabPanel>
+      </Tabs>
+      </div>
+      <Map
+        google = { this.props.google }
+        zoom = { 14 }
+        center = {{ lat: this.state.lat, lng: this.state.lng }}
+        initialCenter = {{ lat: this.state.lat, lng: this.state.lng }}
+        onClick={this.mapClicked}
+      >
+        <InfoWindow
+          visible = { this.state.showingInfoWindow }
+          position = {{ lat: this.state.lat, lng: this.state.lng }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div>
+            <h4>{this.state.city}:{this.state.county}:{this.state.state}:{this.state.country}<br /> 
+        {this.state.date}: {this.state.cases}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: (process.env.REACT_APP_MAP_API_KEY)
+})(GoogleMap);
